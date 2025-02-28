7f0fd10e0741f9f9b4879b8a999f10b5e08674d5
slimafilipe <limafilipe.coding@gmail.com>
2025-02-28 00:08:08 -0300

    Adicionando Desafio3

---
+++ src/desafios/Desafio3.java
@@ -0,0 +1,14 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio3 {
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(-5,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numeros.stream().filter(n -> n > 0).forEach(System.out::println);
+
+    }
+}
